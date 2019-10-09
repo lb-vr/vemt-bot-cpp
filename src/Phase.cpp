f2a287388c12a7268b0c8bfab7728f6c29500e9d
Shirao Shotaro <shirao.shotaro@gmail.com>
2019-10-10 01:29:49 +0900

    add Phase class

---
+++ src/Phase.cpp
@@ -0,0 +1,20 @@
+#include "Phase.hpp"
+
+const vemt::Phase vemt::Phase::kPreEntry = 1;
+const vemt::Phase vemt::Phase::kEntry = 2;
+const vemt::Phase vemt::Phase::kSubmit = 3;
+
+vemt::Phase::Phase(const int phase_int) noexcept : phase_int_(phase_int) {}
+vemt::Phase::Phase(const Phase & phase) noexcept : phase_int_(phase.phase_int_) {}
+vemt::Phase::~Phase() noexcept {}
+const bool vemt::Phase::operator==(const Phase & phase) const noexcept { return this->to_int() == phase.to_int(); }
+const bool vemt::Phase::operator!=(const Phase & phase) const noexcept { return this->to_int() != phase.to_int(); }
+const bool vemt::Phase::operator>(const Phase & phase) const noexcept { return this->to_int() > phase.to_int(); }
+const bool vemt::Phase::operator<(const Phase & phase) const noexcept { return this->to_int() < phase.to_int(); }
+const bool vemt::Phase::operator==(const int phase) const noexcept { return this->to_int() == phase; }
+const bool vemt::Phase::operator!=(const int phase) const noexcept { return this->to_int() != phase; }
+const bool vemt::Phase::operator>(const int phase) const noexcept { return this->to_int() > phase; }
+const bool vemt::Phase::operator<(const int phase) const noexcept { return this->to_int() < phase; }
+int vemt::Phase::to_int(void) const noexcept { return this->phase_int_; }
+vemt::Phase::operator int(void) const noexcept { return this->to_int(); }
+
