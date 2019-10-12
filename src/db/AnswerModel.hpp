9a53a959b12a1c68ff17fa405d2ad541951c9d43
shigeno-y <shigeno.yoshitaka@d.nagoya-u.jp>
2019-10-12 23:21:35 +0900

    add model header

--- src/db/AnswerModel.hpp
+++ src/db/AnswerModel.hpp
@@ -1,11 +1,6 @@
 #ifndef VEMT_DB_ANSWERMODEL_INCLUDED
 #define VEMT_DB_ANSWERMODEL_INCLUDED
-#include <string>
-#include <sstream>
-#include <chrono>
-#include <ctime>
-#include <iomanip>
-#include "../Phase.hpp"
+#include "type/Param.hpp"
 namespace vemt
 {
 namespace db
@@ -14,32 +9,36 @@ namespace db
 class AnswerModel
 {
 public:
-    static const std::string tableName;
-    AnswerModel(
-        long int id,
-        long int discord_uid,
-        std::string package_url,
-        vemt::Phase current_phase,
-        std::chrono::system_clock::time_point created_at,
-        std::chrono::system_clock::time_point updated_at
-    );
-    ~AnswerModel(){}
+	AnswerModel(
+		const vemt::db::type::IntParam & id,
+		const vemt::db::type::IntParam & entry_id,
+		const vemt::db::type::IntParam & question_item_id,
+		const vemt::db::type::StringParam & item_value,
+		const vemt::db::type::DatetimeParam & created_at,
+		const vemt::db::type::DatetimeParam & updated_at
+	);
+	AnswerModel(
+		const vemt::db::type::IntParam & entry_id,
+		const vemt::db::type::IntParam & question_item_id,
+		const vemt::db::type::StringParam & item_value
+	);
+	~AnswerModel(){}
 
-    long int getId();
-    long int getEntryId();
-    long int getQuestionItemId();
-    std::string getItemValue();
-    std::chrono::system_clock::time_point getCreatedAt();
-    std::chrono::system_clock::time_point getUpdatedAt();
-    std::string toString();
+	const int getId();
+	const int getEntryId();
+	const int getQuestionItemId();
+	const std::string getItemValue();
+	std::chrono::system_clock::time_point getCreatedAt();
+	std::chrono::system_clock::time_point getUpdatedAt();
+	std::string toString();
 
 private:
-    long int id;
-    long int entry_id;
-    long int question_item_id;
-    std::string item_value;
-    std::chrono::system_clock::time_point created_at;
-    std::chrono::system_clock::time_point updated_at;
+	const vemt::db::type::IntParam id;
+	vemt::db::type::IntParam entry_id;
+	vemt::db::type::IntParam question_item_id;
+	vemt::db::type::StringParam item_value;
+	vemt::db::type::DatetimeParam created_at;
+	vemt::db::type::DatetimeParam updated_at;
 };
 } // namespace db
 } // namespace vemt
