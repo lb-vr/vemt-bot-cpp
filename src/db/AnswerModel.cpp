aa26cc0671c92856877edad0006f38de0563bdac
shigeno-y <shigeno.yoshitaka@d.nagoya-u.jp>
2019-10-13 17:49:20 +0900

    update models

---
+++ src/db/AnswerModel.cpp
@@ -0,0 +1,66 @@
+﻿#include "AnswerModel.hpp"
+
+vemt::db::AnswerModel::AnswerModel(
+	const vemt::db::type::IntParam & id,
+	const vemt::db::type::IntParam & entry_id,
+	const vemt::db::type::IntParam & question_item_id,
+	const vemt::db::type::StringParam & item_value,
+	const vemt::db::type::DatetimeParam & created_at,
+	const vemt::db::type::DatetimeParam & updated_at
+) noexcept :
+	id_(id),
+	entry_id_(entry_id),
+	question_item_id_(question_item_id),
+	item_value_(item_value),
+	created_at_(created_at),
+	updated_at_(updated_at){
+}
+vemt::db::AnswerModel::AnswerModel(
+	const vemt::db::type::IntParam & entry_id,
+	const vemt::db::type::IntParam & question_item_id,
+	const vemt::db::type::StringParam & item_value
+) noexcept :
+	entry_id_(entry_id),
+	question_item_id_(question_item_id),
+	item_value_(item_value){
+}
+vemt::db::AnswerModel::AnswerModel(const AnswerModel & c) noexcept :
+	id_(c.id_),
+	entry_id_(c.entry_id_),
+	question_item_id_(c.question_item_id_),
+	item_value_(c.item_value_),
+	created_at_(c.created_at_),
+	updated_at_(c.updated_at_){
+}
+
+const int vemt::db::AnswerModel::getId(){
+	return this->id_.get();
+}
+const int vemt::db::AnswerModel::getEntryId(){
+	return this->entry_id_.get();
+}
+const int vemt::db::AnswerModel::getQuestionItemId(){
+	return this->question_item_id_.get();
+}
+const std::string vemt::db::AnswerModel::getItemValue(){
+	return this->item_value_.get();
+}
+const std::chrono::system_clock::time_point vemt::db::AnswerModel::getCreatedAt(){
+	return this->created_at_.get();
+}
+const std::chrono::system_clock::time_point vemt::db::AnswerModel::getUpdatedAt(){
+	return this->updated_at_.get();
+}
+
+const std::string vemt::db::AnswerModel::toString() const {
+	std::stringstream ret;
+    ret << "{model: 'AnswerModel', "
+		<< "id: '" << id_.get() << "', "
+		<< "entry_id: '" << entry_id_.get() << "', "
+		<< "question_item_id: '" << question_item_id_.get() << "', "
+		<< "item_value: '" << item_value_.get() << "', "
+		<< "created_at: '" << created_at_.getAsString() << "', "
+		<< "updated_at: '" << updated_at_.getAsString() << "'"
+		<< "}";
+    return ret.str();
+}
---
+++ src/db/EntryModel.cpp
@@ -0,0 +1,84 @@
+﻿#include "EntryModel.hpp"
+
+vemt::db::EntryModel::EntryModel(
+	const vemt::db::type::IntParam & id,
+	const vemt::db::type::IntParam & discord_uid,
+	const vemt::db::type::StringParam & package_url,
+	const vemt::db::type::IntParam & current_phase,
+	const vemt::db::type::IntParam & query_status_message_id,
+	const vemt::db::type::IntParam & working_status_message_id,
+	const vemt::db::type::DatetimeParam & created_at,
+	const vemt::db::type::DatetimeParam & updated_at
+) noexcept :
+	id_(id),
+	discord_user_id_(discord_uid),
+	package_url_(package_url),
+	current_phase_(current_phase),
+	query_status_message_id_(query_status_message_id),
+	working_status_message_id_(working_status_message_id),
+	created_at_(created_at),
+	updated_at_(updated_at){
+}
+vemt::db::EntryModel::EntryModel(
+	const vemt::db::type::IntParam & discord_uid,
+	const vemt::db::type::StringParam & package_url,
+	const vemt::db::type::IntParam & current_phase,
+	const vemt::db::type::IntParam & query_status_message_id,
+	const vemt::db::type::IntParam & working_status_message_id
+) noexcept :
+	discord_user_id_(discord_uid),
+	package_url_(package_url),
+	current_phase_(current_phase),
+	query_status_message_id_(query_status_message_id),
+	working_status_message_id_(working_status_message_id){
+}
+vemt::db::EntryModel::EntryModel(const EntryModel & c) noexcept :
+	id_(c.id_.get()),
+	discord_user_id_(c.discord_user_id_.get()),
+	package_url_(c.package_url_.get()),
+	current_phase_(c.current_phase_.get()),
+	query_status_message_id_(c.query_status_message_id_.get()),
+	working_status_message_id_(c.working_status_message_id_.get()),
+	created_at_(c.created_at_.get()),
+	updated_at_(c.updated_at_.get()){
+}
+
+int vemt::db::EntryModel::getId(){
+	return this->id_.get();
+}
+int vemt::db::EntryModel::getDiscordUid(){
+	return this->discord_user_id_.get();
+}
+std::string vemt::db::EntryModel::getPackageUrl(){
+	return this->package_url_.get();
+}
+int vemt::db::EntryModel::getCurrentPhase(){
+	return this->current_phase_.get();
+}
+int vemt::db::EntryModel::getQueryStatusMessageId(){
+	return this->query_status_message_id_.get();
+}
+int vemt::db::EntryModel::getWorkingStatusMessageId(){
+	return this->working_status_message_id_.get();
+}
+std::chrono::system_clock::time_point vemt::db::EntryModel::getCreatedAt(){
+	return this->created_at_.get();
+}
+std::chrono::system_clock::time_point vemt::db::EntryModel::getUpdatedAt(){
+	return this->updated_at_.get();
+}
+
+std::string vemt::db::EntryModel::toString() const {
+	std::stringstream ret;
+    ret << "{model: 'EntryModel', "
+		<< "id: '" << id_.get() << "', "
+		<< "discord_user_id: '" << discord_user_id_.get() << "', "
+		<< "package_url: '" << package_url_.get() << "', "
+		<< "current_phase: '" << current_phase_.get() << "', "
+		<< "query_status_message_id: '" << query_status_message_id_.get() << "', "
+		<< "working_status_message_id: '" << working_status_message_id_.get() << "', "
+		<< "created_at: '" << created_at_.getAsString() << "', "
+		<< "updated_at: '" << updated_at_.getAsString() << "'"
+		<< "}";
+    return ret.str();
+}
