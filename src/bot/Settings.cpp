fc7040eb810f86766b2b6b1edff9a80779fc776e
Shirao Shotaro <shirao.shotaro@gmail.com>
2019-10-22 21:16:31 +0900

    Settingsを分かりやすく、シンプルになるよう更新

--- src/bot/Settings.cpp
+++ src/bot/Settings.cpp
@@ -6,43 +6,83 @@
 
 std::unordered_map<int64_t, vemt::bot::Settings> vemt::bot::Settings::server_settings_;
 
-vemt::bot::Settings::Settings(const int64_t serverID, const int64_t bot_category, const int64_t bot_control_channel,
-	const int64_t entry_channel, const int64_t status_channel, const int64_t query_channel, const int64_t exhibitor_category, const int64_t bot_admin_role,
-	const int64_t exhibitor_role, const int64_t manager_role, const int64_t vemt_bot_role, const int64_t everyone_role)
-	: server_id_(serverID), bot_category_(bot_category), bot_control_channel_(bot_control_channel),
-	entry_channel_(entry_channel), status_channel_(status_channel), query_channel_(query_channel),
-	exhibitor_category_(exhibitor_category), bot_admin_role_(bot_admin_role), exhibitor_role_(exhibitor_role),
-	manager_role_(manager_role), vemt_bot_role_(vemt_bot_role), everyone_role_(everyone_role) {}
+vemt::bot::Settings::Settings(
+	const int64_t serverID,
+	const int64_t bot_category,
+	const int64_t bot_control_channel,
+	const int64_t contact_category,
+	const int64_t entry_channel,
+	const int64_t status_channel,
+	const int64_t query_channel,
+	const int64_t bot_admin_role,
+	const int64_t exhibitor_role,
+	const int64_t manager_role,
+	const int64_t vemt_bot_role,
+	const int64_t everyone_role
+):
+	server_id_			(serverID),
+	bot_category_		(bot_category),
+	bot_control_channel_(bot_control_channel),
+	contact_category_	(contact_category),
+	entry_channel_		(entry_channel),
+	status_channel_		(status_channel),
+	query_channel_		(query_channel),
+	bot_admin_role_		(bot_admin_role),
+	exhibitor_role_		(exhibitor_role),
+	manager_role_		(manager_role),
+	vemt_bot_role_		(vemt_bot_role),
+	everyone_role_		(everyone_role)
+{}
 
-vemt::bot::Settings::Settings(const Settings & s) noexcept
-	: server_id_(s.server_id_), bot_category_(s.bot_category_), bot_control_channel_(s.bot_control_channel_),
-	entry_channel_(s.entry_channel_), status_channel_(s.status_channel_), query_channel_(s.query_channel_),
-	exhibitor_category_(s.exhibitor_category_), bot_admin_role_(s.bot_admin_role_), exhibitor_role_(s.exhibitor_role_),
-	manager_role_(s.manager_role_), vemt_bot_role_(s.vemt_bot_role_), everyone_role_(s.everyone_role_) {}
 
-const int64_t vemt::bot::Settings::getServerID() const { return this->server_id_; }
-const int64_t vemt::bot::Settings::getBotCategory() const { return this->bot_category_; }
+const int64_t vemt::bot::Settings::getServerID() const			{ return this->server_id_; }
+
+const int64_t vemt::bot::Settings::getBotCategory() const		{ return this->bot_category_; }
+const int64_t vemt::bot::Settings::getContactCategory() const	{ return this->contact_category_; }
+
 const int64_t vemt::bot::Settings::getBotControlChannel() const { return this->bot_control_channel_; }
-const int64_t vemt::bot::Settings::getEntryChannel() const { return this->entry_channel_; }
-const int64_t vemt::bot::Settings::getStatusChannel() const { return this->status_channel_; }
-const int64_t vemt::bot::Settings::getQueryChannel() const { return this->query_channel_; }
-const int64_t vemt::bot::Settings::getBotAdminRole() const { return this->bot_admin_role_; }
-const int64_t vemt::bot::Settings::getExhibitorRole() const { return this->exhibitor_role_; }
-const int64_t vemt::bot::Settings::getManagerRole() const { return this->manager_role_; }
-const int64_t vemt::bot::Settings::getVemtBotRole() const { return this->vemt_bot_role_; }
-const int64_t vemt::bot::Settings::getEveryoneRole() const { return this->everyone_role_; }
-const int64_t vemt::bot::Settings::getExhibitorCategory() const { return this->exhibitor_category_; }
+const int64_t vemt::bot::Settings::getEntryChannel() const		{ return this->entry_channel_; }
+const int64_t vemt::bot::Settings::getStatusChannel() const		{ return this->status_channel_; }
+const int64_t vemt::bot::Settings::getQueryChannel() const		{ return this->query_channel_; }
+
+const int64_t vemt::bot::Settings::getBotAdminRole() const		{ return this->bot_admin_role_; }
+const int64_t vemt::bot::Settings::getExhibitorRole() const		{ return this->exhibitor_role_; }
+const int64_t vemt::bot::Settings::getManagerRole() const		{ return this->manager_role_; }
+const int64_t vemt::bot::Settings::getVemtBotRole() const		{ return this->vemt_bot_role_; }
+const int64_t vemt::bot::Settings::getEveryoneRole() const		{ return this->everyone_role_; }
+
+const std::vector<int64_t> vemt::bot::Settings::getAllCreatedChannels() const {
+	return std::vector<int64_t>({
+		this->getBotControlChannel(),
+		this->getEntryChannel(),
+		this->getStatusChannel(),
+		this->getQueryChannel()
+		});
+}
+
+const std::vector<int64_t> vemt::bot::Settings::getAllCreatedCategories() const {
+	return std::vector<int64_t>({
+		this->getBotCategory(),
+		this->getContactCategory()
+		});
+}
+
+const std::vector<int64_t> vemt::bot::Settings::getAllCreatedRoles() const {
+	return std::vector<int64_t>({
+		this->getBotAdminRole(),
+		this->getExhibitorRole(),
+		this->getManagerRole()
+		});
+}
 
 const std::string vemt::bot::Settings::_getFilename(const int64_t serverID) {
 	return std::to_string(serverID) + ".json";
 }
 
 vemt::bot::Settings & vemt::bot::Settings::getSettings(const int64_t serverID) {
-
 	if (Settings::server_settings_.count(serverID)) {
 		return Settings::server_settings_.at(serverID);
 	}
-
 	logging::info << "Require server settings. Server ID = " << serverID << std::endl;
 
 	const std::string filename = Settings::_getFilename(serverID);
@@ -62,11 +102,11 @@ vemt::bot::Settings & vemt::bot::Settings::getSettings(const int64_t serverID) {
 		Settings settings = Settings(
 			std::stoll(json["server"].asString()),
 			std::stoll(json["categories"]["bot"].asString()),
+			std::stoll(json["categories"]["exhibitor"].asString()),
 			std::stoll(json["channels"]["bot_control"].asString()),
 			std::stoll(json["channels"]["entry"].asString()),
 			std::stoll(json["channels"]["status"].asString()),
 			std::stoll(json["channels"]["query"].asString()),
-			std::stoll(json["categories"]["exhibitor"].asString()),
 			std::stoll(json["roles"]["bot_admin"].asString()),
 			std::stoll(json["roles"]["exhibitor"].asString()),
 			std::stoll(json["roles"]["manager"].asString()),
@@ -80,7 +120,7 @@ vemt::bot::Settings & vemt::bot::Settings::getSettings(const int64_t serverID) {
 		logging::debug << " -     [CH:entry]       : " << settings.getEntryChannel() << std::endl;
 		logging::debug << " -     [CH:status]      : " << settings.getStatusChannel() << std::endl;
 		logging::debug << " -     [CH:query]       : " << settings.getQueryChannel() << std::endl;
-		logging::debug << " -   [CAT:EXHIBITOR]    : " << settings.getExhibitorCategory() << std::endl;
+		logging::debug << " -   [CAT:EXHIBITOR]    : " << settings.getContactCategory() << std::endl;
 		logging::debug << "-------------------------------------------------------" << std::endl;
 		logging::debug << " - Role [BotAdmin]      : " << settings.getBotAdminRole() << std::endl;
 		logging::debug << " - Role [Exhibitor]     : " << settings.getExhibitorRole() << std::endl;
@@ -96,35 +136,33 @@ vemt::bot::Settings & vemt::bot::Settings::getSettings(const int64_t serverID) {
 	}
 }
 
-vemt::bot::Settings vemt::bot::Settings::createSettings(const int64_t serverID, const int64_t bot_category, const int64_t bot_control_channel, const int64_t entry_channel,
-	const int64_t status_channel, const int64_t query_channel, const int64_t exhibitor_category, const int64_t bot_admin_role, const int64_t exhibitor_role,
-	const int64_t manager_role, const int64_t vemt_bot_role, const int64_t everyone_role) {
-	const std::string filename = Settings::_getFilename(serverID);
+void vemt::bot::Settings::save() const {
+	const std::string filename = Settings::_getFilename(this->getServerID());
 
-	json11::Json _server_id			(std::to_string(serverID));
+	json11::Json _server_id			(std::to_string(this->getServerID()));
 
 	json11::Json _channels({
-		{"bot_control",	json11::Json(std::to_string(bot_control_channel))},
-		{"entry",		json11::Json(std::to_string(entry_channel))},
-		{"status",		json11::Json(std::to_string(status_channel))},
-		{"query",		json11::Json(std::to_string(query_channel))}
+		{"bot_control",	json11::Json(std::to_string(this->getBotControlChannel()))},
+		{"entry",		json11::Json(std::to_string(this->getEntryChannel()))},
+		{"status",		json11::Json(std::to_string(this->getStatusChannel()))},
+		{"query",		json11::Json(std::to_string(this->getQueryChannel()))}
 		});
 
 	json11::Json _categories({
-		{"bot",			json11::Json(std::to_string(bot_category))},
-		{"exhibitor",	json11::Json(std::to_string(exhibitor_category))}
+		{"bot",			json11::Json(std::to_string(this->getBotCategory()))},
+		{"contact",		json11::Json(std::to_string(this->getContactCategory()))}
 		});
 	
 	json11::Json _roles({
-		{"bot_admin",	json11::Json(std::to_string(bot_admin_role))},
-		{"exhibitor",	json11::Json(std::to_string(exhibitor_role))},
-		{"manager",		json11::Json(std::to_string(manager_role))},
-		{"vemt_bot",	json11::Json(std::to_string(vemt_bot_role))},
-		{"everyone",    json11::Json(std::to_string(everyone_role))}
+		{"bot_admin",	json11::Json(std::to_string(this->getBotAdminRole()))},
+		{"exhibitor",	json11::Json(std::to_string(this->getExhibitorRole()))},
+		{"manager",		json11::Json(std::to_string(this->getManagerRole()))},
+		{"vemt_bot",	json11::Json(std::to_string(this->getVemtBotRole()))},
+		{"everyone",    json11::Json(std::to_string(this->getEveryoneRole()))}
 		});
 
 	json11::Json _root({
-		{"server",		json11::Json(std::to_string(serverID))},
+		{"server",		_server_id},
 		{"channels",	_channels},
 		{"categories",	_categories},
 		{"roles",		_roles}
@@ -134,13 +172,9 @@ vemt::bot::Settings vemt::bot::Settings::createSettings(const int64_t serverID, 
 	if (!ofst) throw SettingsNotFoundException("Settings file is not found. PATH = " + filename);
 	ofst << _root.dump();
 
-	Settings settings(
-		serverID, bot_category, bot_control_channel, entry_channel, status_channel, query_channel,
-		exhibitor_category, bot_admin_role, exhibitor_category, manager_role, vemt_bot_role, everyone_role);
-	Settings::server_settings_.emplace(serverID, settings);
-	return settings;
+	Settings::server_settings_.emplace(this->getServerID(), *this);
 }
 
-void vemt::bot::Settings::clearCache() {
-	Settings::server_settings_.clear();
+void vemt::bot::Settings::clearCache(const int64_t serverID) {
+	Settings::server_settings_.erase(serverID);
 }
--- src/bot/Settings.hpp
+++ src/bot/Settings.hpp
@@ -17,72 +17,69 @@ public:
 		using std::runtime_error::runtime_error;
 	};
 
-	Settings(const Settings & settings) noexcept;
+	Settings(
+		const int64_t serverID,
+		const int64_t bot_category,
+		const int64_t contact_category,
+		const int64_t bot_control_channel,
+		const int64_t entry_channel,
+		const int64_t status_channel,
+		const int64_t query_channel,
+		const int64_t bot_admin_role,
+		const int64_t exhibitor_role,
+		const int64_t manager_role,
+		const int64_t vemt_bot_role,
+		const int64_t evetyone_role
+	);
 
+	Settings(const Settings &) noexcept = default;
+
+	// Server
 	const int64_t getServerID() const;
 
+	// Category
 	const int64_t getBotCategory() const;
+	const int64_t getContactCategory() const;
 
+	// Channel
 	const int64_t getBotControlChannel() const;
 	const int64_t getEntryChannel() const;
 	const int64_t getStatusChannel() const;
 	const int64_t getQueryChannel() const;
+
+	// Role
 	const int64_t getBotAdminRole() const;
 	const int64_t getExhibitorRole() const;
 	const int64_t getManagerRole() const;
 	const int64_t getVemtBotRole() const;
 	const int64_t getEveryoneRole() const;
 
-	const int64_t getExhibitorCategory() const;
+	const std::vector<int64_t> getAllCreatedChannels() const;
+	const std::vector<int64_t> getAllCreatedCategories() const;
+	const std::vector<int64_t> getAllCreatedRoles() const;
 
+	void save() const;
 
 	static Settings & getSettings(const int64_t serverID);
 
-	static Settings createSettings(
-		const int64_t serverID,
-		const int64_t bot_category,
-		const int64_t bot_control_channel,
-		const int64_t entry_channel,
-		const int64_t status_channel,
-		const int64_t query_channel,
-		const int64_t exhibitor_category,
-		const int64_t bot_admin_role,
-		const int64_t exhibitor_role,
-		const int64_t manager_role,
-		const int64_t vemt_bot_role,
-		const int64_t evetyone_role
-	);
-
-	static void clearCache();
+	static void clearCache(const int64_t serverID);
 
 private:
 	const int64_t server_id_;
+
 	const int64_t bot_category_;
+	const int64_t contact_category_;
+	
 	const int64_t bot_control_channel_;
 	const int64_t entry_channel_;
 	const int64_t status_channel_;
 	const int64_t query_channel_;
-	const int64_t exhibitor_category_;
+
 	const int64_t bot_admin_role_;
 	const int64_t exhibitor_role_;
 	const int64_t manager_role_;
 	const int64_t vemt_bot_role_;
 	const int64_t everyone_role_;
-
-	Settings(
-		const int64_t serverID,
-		const int64_t bot_category,
-		const int64_t bot_control_channel,
-		const int64_t entry_channel,
-		const int64_t status_channel,
-		const int64_t query_channel,
-		const int64_t exhibitor_category,
-		const int64_t bot_admin_role,
-		const int64_t exhibitor_role,
-		const int64_t manager_role,
-		const int64_t vemt_bot_role,
-		const int64_t evetyone_role
-	);
 
 	static std::unordered_map<int64_t, Settings> server_settings_;
 
